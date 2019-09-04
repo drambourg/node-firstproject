3fbe633d9a189d600c00595637b3b53158a93adc
xNousex <damien.rambourg@gmail.com>
2019-09-04 17:05:17 +0200

    Fix route

--- index.js
+++ index.js
@@ -6,23 +6,6 @@ app.get('/', (request, response) => {
     response.send('Welcome to Express');
 });
 
-app.get('/api/movies', (request, response) => {
-    response.send('All Films');
-});
-
-app.get('/api/movies/:movieid', (request, response) => {
-    response.json({ movie: request.params.movieid });
-});
-
-app.get('/api/employees', (request, response) => {
-    response.sendStatus(404);
-});
-
-app.get('/api/employee', (request, response) => {
-    const name = request.query.name;
-    response.status(404).send('Unable to retrieve employee ' + name);
-});
-
 app.listen(port, (err) => {
     if (err) {
         throw new Error('Something bad happened...');
