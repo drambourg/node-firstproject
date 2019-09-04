2087138836933d51d0e67bd7a20430950b46d1c1
xNousex <damien.rambourg@gmail.com>
2019-09-04 16:46:40 +0200

    Add route to liste all employees

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const connection = require('./config/conf');
 const app = express();
 const port = 3000;
 
 app.get('/', (request, response) => {
     response.send('Welcome to Express');
 });
+
+app.get('/api/employees', (req, res) => {
+    connection.query('SELECT * from employee', (err, results) => {
+        if (err) {
+            res.status(500).send('Error retrieving employees');
+        } else {
+            res.json(results);
+        }
+    });
+});
+
 
 
 app.listen(port, (err) => {
